73a7ed3c51cb8406359647af710f66f90c982bf2
Kaholaz <vkbugge@hotmail.com>
2023-03-06 06:16:06 +0100

    Spring cleaning

--- src/stores/setlist.ts
+++ src/stores/setlist.ts
@@ -5,18 +5,8 @@ import { defineStore } from 'pinia'
 export const useSetListStore = defineStore('setListStore', () => {
     const setList = ref<SetList>(new SetList('New Set List', []))
 
-    function addSong() {
-        const song = new Song('Song Title', 'Artist', 120, 4)
-        setList.value.songs.push(song)
-    }
-
-    function removeSong(index: number) {
-        setList.value.songs.splice(index, 1)
-    }
 
     return {
-        setList,
-        addSong,
-        removeSong,
+        setList
     }
 });
